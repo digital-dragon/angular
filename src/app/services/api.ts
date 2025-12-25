addd0b461298e02f674151abf98f59bba84aa75f
Digital Dragon <rrajendra@yahoo.com>
2025-12-25 12:49:30 -0500

    Some changes

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Picture } from '../types/picture';
 
-export const NASA_API_KEY = 'NyUQfQpUxIA4WZGwHPp8rAKi2pvQHZJgq3iwksZI';
+export const NASA_API_KEY = 'DEMO_KEY';
 export const NASA_BASE_URL = 'https://api.nasa.gov';
 
 @Injectable({
